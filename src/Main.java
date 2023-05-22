6aef0a1ea2e1e04153d5e8bf6d53535253c9dbb2
FelipMa <felipeapenburg@gmail.com>
2023-05-22 12:01:02 -0300

    feat register functions

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,68 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Owner owner = new Owner("João", "12345678910", "123456789", "123456789", new Address("Rua 1", "123", "12345678", "SP", "SP"));
-        Property property = new Property("123456789", "Casa", "Residencial", new Address("Rua 2", "123", "12345678", "SP", "SP"));
-        owner.addProperty(property);
+        Property property = registerProperty();
+        Owner owner = registerOwner();
+        System.out.println(property);
         System.out.println(owner);
+    }
+
+    public static Property registerProperty() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the property iptu: ");
+        String iptu = scanner.nextLine();
+        System.out.println("Enter property address street name: ");
+        String street = scanner.nextLine();
+        System.out.println("Enter property address number: ");
+        String number = scanner.nextLine();
+        System.out.println("Enter property address zip code: ");
+        String zipCode = scanner.nextLine();
+        System.out.println("Enter property address state: ");
+        String state = scanner.nextLine();
+        System.out.println("Enter property address city: ");
+        String city = scanner.nextLine();
+        System.out.println("Enter property type: ");
+        String type = scanner.nextLine();
+        System.out.println("Enter property usage: ");
+        String usage = scanner.nextLine();
+
+        scanner.close();
+
+        Address address = new Address(street, number, zipCode, state, city);
+        Property property = new Property(iptu, type, usage, address);
+        System.out.println("Property registered successfully!");
+
+        return property;
+    }
+
+    public static Owner registerOwner() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter owner name: ");
+        String name = scanner.nextLine();
+        System.out.println("Enter owner cpf: ");
+        String cpf = scanner.nextLine();
+        System.out.println("Enter owner rg: ");
+        String rg = scanner.nextLine();
+        System.out.println("Enter owner phone: ");
+        String phone = scanner.nextLine();
+        System.out.println("Enter owner address street name: ");
+        String street = scanner.nextLine();
+        System.out.println("Enter owner address number: ");
+        String number = scanner.nextLine();
+        System.out.println("Enter owner address zip code: ");
+        String zipCode = scanner.nextLine();
+        System.out.println("Enter owner address state: ");
+        String state = scanner.nextLine();
+        System.out.println("Enter owner address city: ");
+        String city = scanner.nextLine();
+
+        scanner.close();
+
+        Address address = new Address(street, number, zipCode, state, city);
+        Owner owner = new Owner(name, cpf, rg, phone, address);
+        System.out.println("Owner registered successfully!");
+
+        return owner;
     }
 }
